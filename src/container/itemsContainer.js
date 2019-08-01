2258c3280f6477107e954fce52540474206cf88e
Tonyjlum <tonyjlum@gmail.com>
2019-08-01 18:26:18 -0400

    skel item and location container

---
+++ src/container/itemsContainer.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+class ItemContainer extends PureComponent {
+  state = {}
+
+  render(){
+    return (
+      <div>
+        Item Container
+      </div>
+    )
+  }
+
+}
+
+export default ItemContainer
---
+++ src/container/locationContainer.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+class LocationContainer extends PureComponent {
+  state = {}
+
+  render(){
+    return (
+      <div>
+        Location Container
+      </div>
+    )
+  }
+
+}
+
+export default LocationContainer
