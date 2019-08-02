58ca24fd647a8e032709ece3a5612eabccb90a22
Tonyjlum <tonyjlum@gmail.com>
2019-08-02 14:45:46 -0400

    item and location component

---
+++ src/component/item.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+class Item extends PureComponent {
+  state = {}
+
+  render(){
+    return (
+      <div>
+        Item Container
+      </div>
+    )
+  }
+
+}
+
+export default Item
---
+++ src/component/location.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+class Location extends PureComponent {
+  state = {}
+
+  render(){
+    return (
+      <div>
+        Item Container
+      </div>
+    )
+  }
+
+}
+
+export default Location
