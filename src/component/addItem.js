bd8c5f8ac087b17d705bdba595a8371e96d74e48
Tonyjlum <tonyjlum@gmail.com>
2019-08-11 17:13:02 -0400

    add item and location

---
+++ src/component/addItem.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+class AddItem extends PureComponent {
+  state = {}
+
+
+
+  render(){
+    return (
+      <div>
+        Add Item form
+      </div>
+    )
+  }
+
+}
+
+export default AddItem
---
+++ src/component/addLocationModal.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+class AddLocationModal extends PureComponent {
+  state = {}
+
+
+
+  render(){
+    return (
+      <div>
+        AddLocationModal comp
+      </div>
+    )
+  }
+
+}
+
+export default AddLocationModal
